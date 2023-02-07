298a0de038512638123cbeccccb8f61171eb011d
hemanthyk360 <100554513+hemanthyk360@users.noreply.github.com>
2023-02-07 14:12:59 +0530

    Complete JavaScript files
    Basic fundamentals of JS Code. Please put your effort in solving some problems.

---
+++ file.js
@@ -0,0 +1,3 @@
+// document.write("This is external file message")
+console.log("This is external file message")
+document.write("This is document.write message")
---
+++ practice.js
@@ -0,0 +1,5 @@
+std_name = ['faisal','zamir','jafri','codee','jayn','x']
+            //document.write(std_name.length)
+            console.log(std_name.length)
+            console.log(std_name.slice(0,2))
+            console.log(std_name.join("-"))
